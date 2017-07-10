6954ce80e07431089d528a4352d320383eb19279
Ryan Stong <rvanstong@gmail.com>
2017-07-10 15:21:55 -0700

    adds in nav links, needs to be put in navbar

--- application.rb
+++ application.rb
@@ -3,6 +3,19 @@ require 'ruby-hackernews'
 include RubyHackernews
 
 get '/' do
+  @header = "Main"
   @hackernews_entries = Entry.all
+  erb :index
+end
+
+get '/newest' do
+  @header = "Newest"
+  @hackernews_entries = Entry.newest
+  erb :index
+end
+
+get '/jobs' do
+  @header = "Jobs"
+  @hackernews_entries = Entry.jobs
   erb :index
 end
